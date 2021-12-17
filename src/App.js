1ee7241c4249ab9974faa7a6637ee1f552685b4f
hatieudao <froyal2kk1@gmail.com>
2021-12-17 16:30:42 +0700

    init project with react-scripts 4.0.3 to ignore waaning from antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout } from 'antd'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HeaderNav from './components/HeaderNav'
+import HomePage from './pages/HomePage'
+import 'antd/dist/antd.css'
+import './App.css'
+const { Content, Footer } = Layout
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Layout className="layout">
+        <HeaderNav />
+        <Content style={{ padding: '0 50px' }}>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+          </Routes>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/HeaderNav/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Layout, Menu } from 'antd'
+const { Header } = Layout
+function HeaderNav() {
+  return (
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        {new Array(15).fill(null).map((_, index) => {
+          const key = index + 1
+          return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>
+        })}
+      </Menu>
+    </Header>
+  )
+}
+
+export default HeaderNav
